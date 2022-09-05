274c4f3327fbbe15d1a8a2e3f6611ba331709fe9
Jay C <jc30g14@soton.ac.uk>
2022-09-05 20:39:29 +0100

    Code challenge completed with ability to shift or decipher a given string for any positive or negative integer.

---
+++ caesar_Cipher.rb
@@ -0,0 +1,24 @@
+def ceaser_cipher(string, shift)
+    string_array = []
+    for i in 0..(string.length-1)
+        string_array.push(string[i].downcase)
+    end
+    
+    ascii_ordinal = string_array.map do |letter| 
+        if letter.ord < 97 || letter.ord > 122
+            letter.ord
+        elsif (letter.ord + shift < 97) || (letter.ord + shift > 122) 
+                letter = 97 + ((letter.ord + shift - 97) % 26)
+                p letter
+        else letter.ord + shift
+        end
+    end   
+
+    p ascii_ordinal
+    ascii_char = ascii_ordinal.map {|number| number.chr }
+    return ascii_char.join("")
+end    
+
+ceaser_cipher("Abc xyz", -30)
+ceaser_cipher("What a string!", 5)
+
