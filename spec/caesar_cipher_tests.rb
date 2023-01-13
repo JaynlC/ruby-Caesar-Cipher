c16eb9f4685088885cf3e8ccc077807a04eda212
Jay C <jc30g14@soton.ac.uk>
2023-01-13 00:06:20 +0000

    added rspec test cases with all tests passing.

--- spec/caesar_cipher_tests.rb
+++ spec/caesar_cipher_tests.rb
@@ -1,3 +1,19 @@
 require "./lib/caesar_Cipher.rb"
 
-describe
+describe "#caesar_cipher" do
+  it "for given string only, returns shifted string" do
+    expect(caesar_cipher("abc", 2)).to eql("cde")
+  end
+
+  it "is not case sensitive" do
+    expect(caesar_cipher("AbC", 2)).to eql("cde")
+  end
+
+  it "able to return special characters correctly" do
+    expect(caesar_cipher("a!b!c!d$%", 1)).to eql("b!c!d!e$%")
+  end
+
+  it "can wrap from z to a" do
+    expect(caesar_cipher("xyz", 3)).to eql("abc")
+  end
+end
